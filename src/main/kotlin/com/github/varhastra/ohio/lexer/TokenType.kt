20199bae2f74b8fda619b19e518b76d40f1585a2
VarHastra <alex.petrakov.dev@gmail.com>
2019-11-09 23:10:39 +0300

    Introduce more token types

--- src/main/kotlin/com/github/varhastra/ohio/lexer/TokenType.kt
+++ src/main/kotlin/com/github/varhastra/ohio/lexer/TokenType.kt
@@ -4,19 +4,27 @@ enum class TokenType {
 
     // Single character tokens
     LEFT_PAREN,
-    RIGHT_PAREN, SEMICOLON,
+    RIGHT_PAREN,
+    LEFT_BRACE, RIGHT_BRACE,
+    SEMICOLON,
+    PLUS, MINUS, STAR, SLASH, MOD,
 
     // Multiple character tokens
     COLON_EQUAL,
+    EQUAL_EQUAL, BANG_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
 
     // Literals
     IDENTIFIER,
+    NUMBER, STRING,
 
     // Keywords
     NOT,
     AND, NAND, XOR, XNOR, OR, NOR, IMP, NIMP,
     TRUE, FALSE,
     PRINT, PRINTLN,
+    IF, ELSE,
+    WHILE, REPEAT,
 
     EOF
 }
