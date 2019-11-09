bd5a92a9c828dccf67f84f16724ce3cfdf609111
VarHastra <alex.petrakov.dev@gmail.com>
2019-11-09 23:13:07 +0300

    Extend lexer capabilities by adding support for new keywords and string/number literals handling

--- src/main/kotlin/com/github/varhastra/ohio/lexer/Lexer.kt
+++ src/main/kotlin/com/github/varhastra/ohio/lexer/Lexer.kt
@@ -4,6 +4,7 @@ import com.github.varhastra.ohio.common.Position
 import com.github.varhastra.ohio.lexer.TokenType.*
 import com.github.varhastra.ohio.lexer.exceptions.LexException
 import com.github.varhastra.ohio.lexer.exceptions.UnexpectedSymbolException
+import com.github.varhastra.ohio.lexer.exceptions.ValueOutOfRangeException
 
 class Lexer(private val source: String) {
 
@@ -51,10 +52,14 @@ class Lexer(private val source: String) {
 
             if (matcher.lookingAt()) {
                 val lexeme = matcher.group()
-                if (type == IDENTIFIER && lexeme in keywords) {
-                    addToken(keywords.getValue(lexeme), matcher.start() until matcher.end())
-                } else {
-                    addToken(type, matcher.start() until matcher.end())
+                val sourceRange = matcher.start() until matcher.end()
+                when (type) {
+                    NUMBER -> addNumberToken(sourceRange)
+                    STRING -> addStringToken(sourceRange)
+                    IDENTIFIER -> if (lexeme in keywords) {
+                        addToken(keywords.getValue(lexeme), sourceRange)
+                    }
+                    else -> addToken(type, sourceRange)
                 }
                 currentPos = matcher.end()
                 return true
@@ -98,6 +103,29 @@ class Lexer(private val source: String) {
         tokens.add(Token(type, lexeme, literal, currentLine, columnRange, range))
     }
 
+    private fun addNumberToken(range: IntRange) {
+        val literal = source.substring(range).filter { it != '_' }
+        try {
+            addToken(NUMBER, range, literal.toLong())
+        } catch (e: NumberFormatException) {
+            val columnRange = sourceRangeToColumnRange(range)
+            val position = Position(currentLine, columnRange)
+            _lexExceptions.add(
+                ValueOutOfRangeException(
+                    position,
+                    "The value is out of range. Must be in [-2^63+1..2^63-1]."
+                )
+            )
+        }
+    }
+
+    private fun addStringToken(range: IntRange) {
+        val s = range.first + 1
+        val e = range.last - 1
+        val literal = source.substring(s..e)
+        addToken(STRING, range, literal)
+    }
+
     private fun addEofToken() {
         tokens.add(Token(EOF, "", null, currentLine, IntRange.EMPTY, IntRange.EMPTY))
     }
@@ -119,14 +147,29 @@ class Lexer(private val source: String) {
         private val tokenPatterns = mapOf(
             "[(]".toPattern() to LEFT_PAREN,
             "[)]".toPattern() to RIGHT_PAREN,
+            "[{]".toPattern() to LEFT_BRACE,
+            "[}]".toPattern() to RIGHT_BRACE,
+            "[+]".toPattern() to PLUS,
+            "-".toPattern() to MINUS,
+            "[*]".toPattern() to STAR,
+            "/".toPattern() to SLASH,
+            "%".toPattern() to MOD,
             ";".toPattern() to SEMICOLON,
+            "==".toPattern() to EQUAL_EQUAL,
+            "!=".toPattern() to BANG_EQUAL,
             ":=".toPattern() to COLON_EQUAL,
+            ">=".toPattern() to GREATER_EQUAL,
+            "<=".toPattern() to LESS_EQUAL,
+            ">".toPattern() to GREATER,
+            "<".toPattern() to LESS,
             "[_a-zA-Z][_a-zA-Z0-9]*".toPattern() to IDENTIFIER,
-            "1".toPattern() to TRUE,
-            "0".toPattern() to FALSE
+            "[0-9][_0-9]*".toPattern() to NUMBER,
+            "\"[^\n]*\"".toPattern() to STRING
         )
 
         private val keywords = mapOf(
+            "true" to TRUE,
+            "false" to FALSE,
             "print" to PRINT,
             "println" to PRINTLN,
             "and" to AND,
@@ -137,7 +180,11 @@ class Lexer(private val source: String) {
             "nor" to NOR,
             "imp" to IMP,
             "nimp" to NIMP,
-            "not" to NOT
+            "not" to NOT,
+            "if" to IF,
+            "else" to ELSE,
+            "while" to WHILE,
+            "repeat" to REPEAT
         )
 
         private val newlinePattern = "\n".toPattern()
---
+++ src/main/kotlin/com/github/varhastra/ohio/lexer/exceptions/ValueOutOfRangeException.kt
@@ -0,0 +1,11 @@
+package com.github.varhastra.ohio.lexer.exceptions
+
+import com.github.varhastra.ohio.common.Position
+
+class ValueOutOfRangeException(
+    positionInTheSource: Position,
+    message: String,
+    cause: Throwable? = null,
+    enableSuppression: Boolean = false,
+    writableStackTrace: Boolean = true
+) : LexException(positionInTheSource, message, cause, enableSuppression, writableStackTrace)
